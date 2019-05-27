81ce94181ed1f144d9e6dca6feb7df6f44502a07
Toropon <duck7quick@gmail.com>
2019-05-27 13:04:13 +0900

    seedが更新された時に新しく加わったデータの追加方法を記述

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,5 @@
+# 通常のテストデータ
+=begin
 User.create!(
   name: 'admin',
   email: 'admin@example.com',
@@ -5,3 +7,15 @@ User.create!(
   password: 'password',
   password_confirmation: 'password'
 )
+=end
+
+# 指定のメールアドレスのレコードが存在するか調べて、存在しない時だけUserレコードを登録する
+# find_or_create_by!メソッドは引数に渡した属性で検索を行い、レコードが見つかればそれを返し、
+# 見つからなければ新たにレコードを作成する。
+# 検索に用いる属性と登録に用いる属性が異なる為、差分をブロック(do〜end)で指定している。
+User.find_or_create_by!(email: 'admin@example.com') do | user |
+  user.name = 'admin'
+  user.admin = true
+  user.password = 'password'
+  user.password_confirmation = 'password'
+end
